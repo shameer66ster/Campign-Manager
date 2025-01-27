2659b0c8e3fad0f169e3f61588ea91f6fbca4cd1
Shameer Ebrahim <shameer66ster@gmail.com>
2025-01-27 22:04:08 +0530

    dynamic port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 app.get("/", (req, res) => {
   res.send({ hi: "there" });
 });
-const port = 5000;
-app.listen(port, () => {
-  console.log(`Server is listning to port ${port}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is listning to port ${PORT}`);
 });
